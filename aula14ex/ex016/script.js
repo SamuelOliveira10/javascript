f56585ac4c18d25edd450cbb76caf76b4fc2b6d6
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-02-28 00:27:58 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,23 @@
 function contar() {
-    var ni = Number(document.getElementById('ini').value)
-    var nf = Number(document.getElementById('fim').value)
-    var np = Number(document.getElementById('step').value)
+    var ni = document.getElementById('ini').value
+    var nf = document.getElementById('fim').value
+    var np = document.getElementById('step').value
     var res = document.getElementById('msg')
-
-    res.innerHTML = `Você quer contar do número ${ni} até o número ${nf} de ${np} em ${np}`
+    //res = ''
+    
+    if(ni.length == 0 || nf.length == 0 || np.length == 0){
+        res.innerHTML = `IMPOSSÍVEL CONTAR`
+    } 
+    else if (np == 0){
+        alert('Passo inválido! Considerando PASSO 1')
+        np = 1
+    } 
+    else if (np != 0){  
+        var n1 = Number(ni)
+        var n2 = Number(nf)
+        var n3 = Number(np)
+        for(n1; n1 <= n2; n1 += n3) {
+            res.innerHTML += `${n1} -> `
+        }
+    }
 }
