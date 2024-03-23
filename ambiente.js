95d58430f0a65e821564b1fdfca74ae1045eda4d
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-03-23 14:02:33 -0300

    comecei anotação 01

--- ambiente.js
+++ ambiente.js
@@ -1,6 +1,15 @@
-const fruits = [];
-fruits.push("banana", "apple", "peach");
-fruits[5] = "mango";
-console.log(fruits[5]); // 'mango'
-console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
-console.log(fruits.length); // 6
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+const iterator = colors.keys();
+for (const key of iterator) {
+  console.log(`${key}: ${colors[key]}`);
+}
+// Output
+// 0: red
+// 1: yellow
+// 2: blue
+// 3: undefined
+// 4: undefined
+// 5: purple
+
+const newColors = colors.toReversed() // ['purple', undefined, undefined, 'blue', 'yellow', 'red'
--- anotações/arrays/anotacao01.js
+++ anotações/arrays/anotacao01.js
@@ -1,11 +1,6 @@
-/*              DESCRIÇÃO               */
+/*              DESCRIÇÃO               */ {
 
-/* Índices de array: */{
+/* Índices de array: */
     const years = new Array(1, 2, 3)
     console.log(years["2"] !== years["02"]);
 }
-
-/* Comprimento e propriedades numéricas: */ {
-
-}
-
