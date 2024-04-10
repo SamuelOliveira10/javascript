4179110762f0d6cf8d83b3d58a7bc5c5ae5956d5
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-04-10 10:47:46 -0300

    pratiquei exercicios do code wars

--- ambiente.js
+++ ambiente.js
@@ -2,14 +2,9 @@
 
 function well(x){
   const boas = x.filter((item) => item == 'good')
-  const scrr = boas.length
-  if (scrr > 0 && scrr < 3) {
-    return 'Publish!'
-  } else if (scrr > 2) {
-    return 'I smell a series!'
-  }// else if (scrr < 1) {
-  //   return 'Fail!'
-  // }
+  if (boas.length > 0 && boas.length < 3) return 'Publish!'
+  else if (boas.length > 2) return 'I smell a series!'
+  else if (boas.length < 1) return 'Fail!'
 }
-
+const ideias = ['goo', 'goo', 'goo']
 console.log(well(ideias))
