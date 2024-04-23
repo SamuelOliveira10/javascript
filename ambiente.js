152abaa39700e9ace6f1ac0fd310bd1d4f0862f2
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-04-22 23:14:54 -0300

    iniciei criação da minha primeira calculadora

--- ambiente.js
+++ ambiente.js
@@ -1,10 +1,85 @@
+// function rpsls(pl1,pl2){
+//   const scissors = ['paper', 'lizard']
+//   const paper = ['rock', 'spock']
+//   const rock = ['scissors', 'lizard']
+//   const lizard = ['spock', 'paper']
+//   const spock = ['scissors', 'rock']
+//   if (pl1 == pl2) return 'Draw!'
+//   else if () return 'Player 1 Won!'
+//   else if () return 'Player 2 Won!'
+// }
 
 
-function well(x){
-  const boas = x.filter((item) => item == 'good')
-  if (boas.length > 0 && boas.length < 3) return 'Publish!'
-  else if (boas.length > 2) return 'I smell a series!'
-  else if (boas.length < 1) return 'Fail!'
+function rpsls(pl1, pl2) {
+
 }
-const ideias = ['goo', 'goo', 'goo']
-console.log(well(ideias))
+
+function conv(p1 ,p2){
+    let v1 = 0
+    let v2 = 0
+    if (p1 == 'scissors') v1 = 1
+    if (p1 == 'paper') v1 = 2
+    //if (p1 == 'rock') 
+}
+
+
+
+
+
+
+
+
+var agora = new Date()
+var sem = agora.getDay()
+
+switch (sem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('sábado')
+        break
+    default:
+        console.log('[ERRO] Dia inválido')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const scissors = ['paper', 'lizard']
+const paper = ['rock', 'spock']
+const rock = ['scissors', 'lizard']
+const lizard = ['spock', 'paper']
+const spock = ['scissors', 'rock']
---
+++ calculadora/calc.js
@@ -0,0 +1,9 @@
+function enterEffect(){
+    const parEnt = document.getElementById('parent')
+    parEnt.style.border = '2px solid white'
+}
+
+function outEffect(){
+    const parEnt = document.getElementById('parent')
+    parEnt.style.border = '2px solid black'
+}
