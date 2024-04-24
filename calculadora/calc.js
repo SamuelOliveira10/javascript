5c840c4c2e1f35f8adeddd86af5caf2a001b94a3
SamuelOliveira10 <samueloliveirafn7@gmail.com>
2024-04-24 11:28:43 -0300

    desenvolvendo projeto calculadora

--- calculadora/calc.js
+++ calculadora/calc.js
@@ -1,9 +1,22 @@
-function enterEffect(){
-    const parEnt = document.getElementById('parent')
-    parEnt.style.border = '2px solid white'
+var numero
+function insert(num) {
+    const result = document.getElementById('conteudo')
+    if (num == '.'){
+        if (numero === undefined){
+            result.innerHTML = '0.'
+            numero = '0.'
+        } else if (numero.indexOf('.') == -1){
+            result.innerHTML += '.'
+            numero += '.'
+        }
+    } else {
+        result.innerHTML += num
+        numero += num
+    }
 }
 
-function outEffect(){
-    const parEnt = document.getElementById('parent')
-    parEnt.style.border = '2px solid black'
+function clean() {
+    const result = document.getElementById('conteudo')
+    result.innerHTML = ''
+    var numero = ''
 }
